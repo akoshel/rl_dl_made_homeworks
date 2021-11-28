0c884b9ee7a0f77c6117875524f584e53f4ed764
akoshel <you@example.com>
2021-11-28 16:53:34 +0300

    display fixed

--- HW3/utils.py
+++ HW3/utils.py
@@ -4,11 +4,12 @@ import torch
 
 def display_progress(cond: torch.Tensor, fake: torch.Tensor, real: torch.Tensor, figsize=(10, 12)) -> None:
     for i in range(len(cond)):
-        cond = cond.detach().cpu().permute(1, 2, 0)
-        fake = fake.detach().cpu().permute(1, 2, 0)
-        real = real.detach().cpu().permute(1, 2, 0)
+        cond_show = cond[i].detach().cpu().permute(1, 2, 0)
+        fake_show = fake[i].detach().cpu().permute(1, 2, 0)
+        real_show = real[i].detach().cpu().permute(1, 2, 0)
         fig, ax = plt.subplots(i, 3, figsize=(10, 15))
-        ax[i][0].imshow(cond)
-        ax[i][2].imshow(fake)
-        ax[i][1].imshow(real)
+        ax[i][0].imshow(cond_show)
+        ax[i][2].imshow(fake_show)
+        ax[i][1].imshow(real_show)
     plt.show()
+    
